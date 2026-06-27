fb3c60a16c659671b368b164ee085fd2be5c72ba
Manjas Anand <manjasanand08@gmail.com>
2026-06-27 15:21:42 +0530

    Add operator examples and user input in Day2.py
    Introduced various operator examples and user input handling.

---
+++ Day2.py
@@ -0,0 +1,46 @@
+#operators and user input
+
+print ("There are 6 types of operators:") print ("1. Arithmetic operators")
+print ("2. Comparison operators")
+print("3. Logical operators")
+print("4. Assignment operators")
+print("5. Identity operators")
+print ("6. Membership operators")
+
+print ("Types of
+arithmetic operators:")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+print("5. Modulus (%) ")
+print("6. Exponentiation(**)")
+
+a=7+5
+print (a)
+
+b=7-5
+print (b)
+
+C=7*5
+print (c)
+
+d=7/5
+print (d)
+
+e=7%5
+print (e)
+
+f=7**5
+print (f)
+
+g=7/15
+print (g)
+
+print("User input:")
+name = str(input ("What
+is your name? "))
+print("Hello, " + name
++ "!")
+
+print ("Bye!")
