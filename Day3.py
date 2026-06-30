315d4aec8ddc6fc8afec86b073757c0ccf0f5a06
Manjas Anand <manjasanand08@gmail.com>
2026-06-30 21:23:38 +0530

    Add conditional statements and simple calculator

---
+++ Day3.py
@@ -0,0 +1,10 @@
+print ("Conditional statements in Python")
+x = 5
+if X > 0:
+print ("x is positive")
+elif x == 0:
+print("x is neither positive nor negative")
+else:
+print ("x is not positive")
+
+#Simple Calculator
